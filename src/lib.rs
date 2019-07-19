90fcc45fd7d92f03ac0bef1e877e0d265729aa53
Piet Geursen <pietgeursen@gmail.com>
2019-07-19 16:31:00 +1200

    Change what is exported at the top level of the module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,17 @@
 #[macro_use]
 extern crate serde_derive;
-
 extern crate serde_json;
 
 pub mod entry;
 pub mod entry_store;
-mod hex_serde;
 pub mod log;
 pub mod memory_entry_store;
 pub mod signature;
 pub mod yamf_hash;
 pub mod yamf_signatory;
 
-pub use entry::{Entry, Error as EntryError};
-pub use entry_store::{EntryStore, Error as EntryStoreError};
+mod hex_serde;
+
+pub use entry::{Entry};
+pub use entry_store::{EntryStore};
 pub use log::Log;
-pub use memory_entry_store::MemoryEntryStore;
-pub use signature::Signature;
-pub use yamf_hash::YamfHash;
-pub use yamf_signatory::YamfSignatory;
