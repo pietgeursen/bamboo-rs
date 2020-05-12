d3c4b2170298bc7c6ef6b76bf09c9e2469a45b44
Piet Geursen <pietgeursen@gmail.com>
2020-05-12 18:05:28 +0200

    Renable ios builds, copy headerfile when deploying.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,14 +17,15 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
+    # TODO generate the c headerfile and copy it too.
     if [ -z $IS_NO_STD ]
     then
       cross build --target $TARGET --release
       cp target/$TARGET/release/bamboo-cli $stage/ || true
-      cp target/$TARGET/release/bamboo-cli.exe $stage/ || true
+      cp target/$TARGET/release/bamboo-cli.exe $stage/ || true # windows
       cp target/$TARGET/release/libbamboo_core.a $stage/ || true
       cp target/$TARGET/release/libbamboo_core.so $stage/ || true # this would fail for musl builds so fail quietly
-      cp target/$TARGET/release/libbamboo_core.dll $stage/ || true # if we're windows 
+      cp target/$TARGET/release/bamboo_core.dll $stage/ || true # if we're windows 
     else
       cd bamboo-core
       cross build -p bamboo-core --target $TARGET --release --no-default-features
@@ -32,7 +33,7 @@ main() {
       cd ..
     fi
 
-    #strip target/$TARGET/release
+    cp $src/bamboo-core/libbamboo.h $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
