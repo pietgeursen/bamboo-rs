6a6069137a951722b6594752b8abd5fe1ef9683a
Piet Geursen <pietgeursen@gmail.com>
2020-05-24 16:18:01 +0200

    Ugh, just use old deploy script.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,24 +18,22 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO generate the c headerfile and copy it too.
-    cp $src/bamboo-core/libbamboo.h $stage/
-
-
-    cd bamboo-core
-    cross build -p bamboo-core --target $TARGET --release --no-default-features
-    cp target/$TARGET/release/libbamboo_core.a $stage/ || true
-    cp target/$TARGET/release/libbamboo_core.so $stage/ || true # this would fail for musl builds so fail quietly
-    cp target/$TARGET/release/bamboo_core.dll $stage/ || true # if we're windows 
-    cd ..
-
     if [ -z $IS_NO_STD ]
     then
-      cd bamboo-cli
       cross build --target $TARGET --release
-      cd ..
       cp target/$TARGET/release/bamboo-cli $stage/ || true
       cp target/$TARGET/release/bamboo-cli.exe $stage/ || true # windows
+      cp target/$TARGET/release/libbamboo_core.a $stage/ || true
+      cp target/$TARGET/release/libbamboo_core.so $stage/ || true # this would fail for musl builds so fail quietly
+      cp target/$TARGET/release/bamboo_core.dll $stage/ || true # if we're windows 
+    else
+      cd bamboo-core
+      cross build -p bamboo-core --target $TARGET --release --no-default-features
+      cp target/$TARGET/release/libbamboo_core.a $stage/
+      cd ..
     fi
+
+    cp $src/bamboo-core/libbamboo.h $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
