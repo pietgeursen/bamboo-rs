305fc35f332e2c967c055ab93e9f524650bdfe3f
Piet Geursen <pietgeursen@gmail.com>
2021-01-05 15:31:50 +1300

    add more thoughts

--- bamboo-log/src/log/add.rs
+++ bamboo-log/src/log/add.rs
@@ -1,9 +1,23 @@
+use bamboo_core::Entry;
 use super::Log;
 use crate::entry_store::EntryStorer;
 use bamboo_core::entry::decode;
 use bamboo_core::entry::verify;
 use bamboo_core::error::*;
 use bamboo_core::lipmaa;
+use std::convert::TryFrom;
+
+#[derive(Clone)]
+pub struct EntryAndPayload<'a, 'b>{
+    entry: &'a [u8],
+    payload: Option<&'b [u8]>
+}
+
+struct EntryAndPayloadAndDecodedEntry<'a, 'b>{
+    entry: &'a [u8],
+    payload: Option<&'b [u8]>,
+    decoded_entry: Entry<&'a [u8], &'a [u8] >
+}
 
 impl<Store: EntryStorer> Log<Store> {
     /// Add a valid message to the Log.
@@ -27,7 +41,26 @@ impl<Store: EntryStorer> Log<Store> {
     // Need to decide how to handle broken feeds.
     // - Should entry store expose methods to record broken feeds?
     // - Should entry store expose stuff to handle storing payloads too which can be overridden?
-    pub fn add_batch(&mut self, entry_bytes: &[u8], payload: Option<&[u8]>) -> Result<()> {
+    //
+    pub fn add_batch(&mut self, entries_and_payloads: &[EntryAndPayload]) -> Result<()> {
+        let mut sorted_entries_and_payloads = Vec::new();
+        sorted_entries_and_payloads.extend_from_slice(entries_and_payloads);
+
+        sorted_entries_and_payloads
+            .iter()
+            .map(|entry_and_payload|{
+                let decoded = Entry::try_from(entry_and_payload.entry)?;
+                Ok(EntryAndPayloadAndDecodedEntry{
+                    entry: entry_and_payload.entry,
+                    payload: entry_and_payload.payload,
+                    decoded_entry: decoded
+                })
+            })
+            .sort_unstable_by(|entry_and_payload|{
+                let entry = decode(entry_bytes).map_err(|_| Error::AddEntryDecodeFailed)?;
+
+            });
+
         // Decode the entry that we want to add.
         let entry = decode(entry_bytes).map_err(|_| Error::AddEntryDecodeFailed)?;
 
