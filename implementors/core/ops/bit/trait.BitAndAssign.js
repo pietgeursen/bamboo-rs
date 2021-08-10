98cf20550bf5e16cd3ec592a6817276bbd610e02
pietgeursen <pietgeursen@users.noreply.github.com>
2021-08-10 08:34:45 +0000

    deploy: 851cdbbfaedef9a92403bd52e7cf7960dbf66bd8

--- implementors/core/ops/bit/trait.BitAndAssign.js
+++ implementors/core/ops/bit/trait.BitAndAssign.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["subtle"] = [{"text":"impl BitAndAssign&lt;Choice&gt; for Choice","synthetic":false,"types":[]}];
+implementors["subtle"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a>&lt;<a class=\"struct\" href=\"subtle/struct.Choice.html\" title=\"struct subtle::Choice\">Choice</a>&gt; for <a class=\"struct\" href=\"subtle/struct.Choice.html\" title=\"struct subtle::Choice\">Choice</a>","synthetic":false,"types":["subtle::Choice"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
